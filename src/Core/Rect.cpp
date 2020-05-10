05107d746f6a9ab68eda02b70f9d81cbb5f2b997
Sebastian Kylander <developer@gaztin.com>
2020-05-10 20:34:37 +0200

    Add Point and Rect classes

---
+++ src/Core/Rect.cpp
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2020 Sebastian Kylander https://gaztin.com/
+ *
+ * This software is provided 'as-is', without any express or implied warranty. In no event will
+ * the authors be held liable for any damages arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose, including commercial
+ * applications, and to alter it and redistribute it freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not claim that you wrote the
+ *    original software. If you use this software in a product, an acknowledgment in the product
+ *    documentation would be appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be misrepresented as
+ *    being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+
+#include "Rect.h"
+
+GENO_NAMESPACE_BEGIN
+
+Rect::Rect( uint32_t width, uint32_t height )
+	: min( 0, 0 )
+	, max( width, height )
+{
+}
+
+Rect::Rect( Point min, Point max )
+	: min( min )
+	, max( max )
+{
+}
+
+uint32_t Rect::Width( void ) const
+{
+	return ( uint32_t )( max.x - min.x );
+}
+
+uint32_t Rect::Height( void ) const
+{
+	return ( uint32_t )( max.y - min.y );
+}
+
+GENO_NAMESPACE_END
