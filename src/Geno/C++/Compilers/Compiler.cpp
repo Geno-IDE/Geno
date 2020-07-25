4f3d3a9552660edcd04f256774c09d7b11a7cf41
Sebastian Kylander <developer@gaztin.com>
2020-07-25 21:54:10 +0200

    Avoid unnecessary calls to `GetStdHandle`

--- src/Geno/C++/Compilers/Compiler.cpp
+++ src/Geno/C++/Compilers/Compiler.cpp
@@ -52,19 +52,22 @@ bool Compiler::Compile( std::wstring_view cpp )
 
 	startup_info.cb          = sizeof( STARTUPINFO );
 	startup_info.wShowWindow = SW_HIDE;
-	startup_info.hStdInput   = GetStdHandle( STD_INPUT_HANDLE );
-	startup_info.hStdOutput  = GetStdHandle( STD_OUTPUT_HANDLE );
-	startup_info.hStdError   = GetStdHandle( STD_ERROR_HANDLE );
 	startup_info.dwFlags     = STARTF_USESHOWWINDOW | STARTF_USESTDHANDLES;
 
 	if( int in = _fileno( stdin ); in > 0 )
 		startup_info.hStdInput = ( HANDLE )_get_osfhandle( in );
+	else
+		startup_info.hStdInput = GetStdHandle( STD_INPUT_HANDLE );
 
 	if( int out = _fileno( stdout ); out > 0 )
 		startup_info.hStdOutput = ( HANDLE )_get_osfhandle( out );
+	else
+		startup_info.hStdOutput = GetStdHandle( STD_OUTPUT_HANDLE );
 	
 	if( int err = _fileno( stderr ); err > 0 )
 		startup_info.hStdError = ( HANDLE )_get_osfhandle( err );
+	else
+		startup_info.hStdError = GetStdHandle( STD_ERROR_HANDLE );
 
 	if( !CreateProcessW( NULL, &command_line[ 0 ], NULL, NULL, TRUE, 0, NULL, NULL, &startup_info, &process_info ) )
 	{
