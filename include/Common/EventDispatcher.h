da1baa6931010362a54e0509fb80611a3ea67668
Sebastian Kylander <developer@gaztin.com>
2020-08-26 14:53:12 +0200

    Fix event dispatcher thread safety

--- include/Common/EventDispatcher.h
+++ include/Common/EventDispatcher.h
@@ -20,6 +20,7 @@
 #include "Common/TypeTraits.h"
 
 #include <functional>
+#include <mutex>
 #include <tuple>
 
 template< typename Derived, typename... Types >
@@ -31,6 +32,11 @@ public:
 
 public:
 
+	template< typename T >
+	using Function = std::function< void( T ) >;
+
+public:
+
 	EventDispatcher( void )              = default;
 	EventDispatcher( EventDispatcher&& ) = default;
 
@@ -38,6 +44,9 @@ public:
 
 public:
 
+	/*
+	 * Subscribe to event persistently (forever)
+	 */
 	template<
 	  typename Functor,
 	  typename = typename std::enable_if_t<
@@ -46,28 +55,68 @@ public:
 	>
 	Derived& operator<<=( Functor&& functor )
 	{
-		using T = std::remove_const_t< std::remove_reference_t< FirstArgumentType< Functor > > >;
+		return Subscribe< Functor >( std::forward< Functor >( functor ), true );
+	}
 
-		SubscriberVector< T >& vec = std::get< SubscriberVector< T > >( subscribers_ );
-		Subscriber< T >        new_subscriber;
-
-		new_subscriber.function = functor;
-
-		vec.emplace_back( std::move( new_subscriber ) );
-
-		return *static_cast< Derived* >( this );
+	/*
+	 * Subscribe to event non-persistently (only once)
+	 */
+	template<
+	  typename Functor,
+	  typename = typename std::enable_if_t<
+	    ( std::is_same_v< Types, std::remove_const_t< std::remove_reference_t< FirstArgumentType< Functor > > > > && ... )
+	  >
+	>
+	Derived& operator^=( Functor&& functor )
+	{
+		return Subscribe< Functor >( std::forward< Functor >( functor ), false );
 	}
 
 protected:
 
 	template< typename T >
-	void Publish( const T& e ) const
+	void Publish( const T& e )
 	{
-		const SubscriberVector< T >& vec = std::get< SubscriberVector< T > >( subscribers_ );
+		SubscriberVector< T >   vec_copy;
+		std::vector< uint64_t > non_persistent_subscriber_ids;
 
-		for( const Subscriber< T >& sub : vec )
 		{
-			sub.function( e );
+			/** 
+			 * We make a copy of the subscriber vector here in case any of the functors called adds another subscriber.
+			 * This is a problem for two reasons.
+			 * 1) It may resize the vector, in which case the iterator will be invalidated.
+			 * 2) It will send *this* event to the new subscriber even though they subscribed after the event was published.
+			 */
+			MutexLock lock( mutex_ );
+			vec_copy = std::get< SubscriberVector< T > >( subscribers_ );
+		}
+
+		for( auto it = vec_copy.begin(); it != vec_copy.end(); ++it )
+		{
+			it->function( e );
+
+			if( !it->persistent )
+				non_persistent_subscriber_ids.push_back( it->id );
+		}
+
+//////////////////////////////////////////////////////////////////////////
+
+		MutexLock              lock( mutex_ );
+		SubscriberVector< T >& vec_ref = std::get< SubscriberVector< T > >( subscribers_ );
+
+		for( uint64_t id : non_persistent_subscriber_ids )
+		{
+			// Find the subscriber to be removed from the function pointer
+			auto it = std::find_if( vec_ref.begin(), vec_ref.end(),
+				[ id ]( const Subscriber< T >& s )
+				{
+					return ( s.id == id );
+				}
+			);
+
+			// Remove the subscriber
+			if( it != vec_ref.end() )
+				vec_ref.erase( it );
 		}
 	}
 
@@ -76,14 +125,41 @@ private:
 	template< typename T >
 	struct Subscriber
 	{
-		std::function< void( T ) > function;
+		Function< T > function;
+		uint64_t      id;
+		bool          persistent;
 	};
 
 	template< typename T >
 	using SubscriberVector = std::vector< Subscriber< T > >;
+	using SubscribersTuple = std::tuple< SubscriberVector< Types >... >;
+	using Mutex            = std::recursive_mutex;
+	using MutexLock        = std::scoped_lock< Mutex >;
 
 private:
 
-	std::tuple< SubscriberVector< Types >... > subscribers_;
+	template< typename Functor >
+	Derived& Subscribe( Functor&& functor, bool persistent )
+	{
+		using T = std::remove_const_t< std::remove_reference_t< FirstArgumentType< Functor > > >;
+
+		MutexLock              lock( mutex_ );
+		SubscriberVector< T >& vec = std::get< SubscriberVector< T > >( subscribers_ );
+		Subscriber< T >        new_subscriber;
+
+		new_subscriber.function   = functor;
+		new_subscriber.id         = ++next_id_;
+		new_subscriber.persistent = persistent;
+
+		vec.emplace_back( std::move( new_subscriber ) );
+
+		return *static_cast< Derived* >( this );
+	}
+
+private:
+
+	SubscribersTuple subscribers_;
+	Mutex            mutex_;
+	uint64_t         next_id_ = 0;
 
 };
