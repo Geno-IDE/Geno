f81bed0975650bbd85bd3db328b952107ca8631f
Sebastian Kylander <developer@gaztin.com>
2021-04-08 16:31:19 +0200

    Bump license year

--- include/Common/Platform/Win32/Win32ProcessInfo.h
+++ include/Common/Platform/Win32/Win32ProcessInfo.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Sebastian Kylander https://gaztin.com/
+ * Copyright (c) 2021 Sebastian Kylander https://gaztin.com/
  *
  * This software is provided 'as-is', without any express or implied warranty. In no event will
  * the authors be held liable for any damages arising from the use of this software.
