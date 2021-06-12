98ad0cdce07deff73a0776a36b14b1f7ae20273f
Sebastian Kylander <developer@gaztin.com>
2021-06-12 23:26:31 +0200

    Add stb_image

--- premake5.lua
+++ premake5.lua
@@ -28,6 +28,7 @@ app( 'Geno' )
 		'third_party/glew/include',
 		'third_party/glfw/include',
 		'third_party/imgui',
+		'third_party/stb',
 	}
 	libdirs {
 		'third_party/glew/lib/Release/%{cfg.platform}'
