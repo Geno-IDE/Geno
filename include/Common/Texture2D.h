391211eff591163d1a38d3dc6b45c440f306c23c
Sebastian Kylander <developer@gaztin.com>
2021-06-12 23:27:42 +0200

    Track texture width and height

--- include/Common/Texture2D.h
+++ include/Common/Texture2D.h
@@ -18,6 +18,8 @@
 #pragma once
 #include <GL/glew.h>
 
+#include <cstdint>
+
 class Texture2D
 {
 public:
@@ -30,7 +32,10 @@ public:
 
 //////////////////////////////////////////////////////////////////////////
 
-	GLuint GetID( void ) const { return m_ID; }
+	GLuint   GetID         ( void ) const { return m_ID; }
+	uint16_t GetWidth      ( void ) const { return m_Width; }
+	uint16_t GetHeight     ( void ) const { return m_Height; }
+	float    GetAspectRatio( void ) const { return static_cast< float >( m_Width ) / m_Height; }
 
 //////////////////////////////////////////////////////////////////////////
 
@@ -40,6 +45,9 @@ private:
 
 //////////////////////////////////////////////////////////////////////////
 
-	GLuint m_ID = 0;
+	GLuint   m_ID     = 0;
+
+	uint16_t m_Width  = 0;
+	uint16_t m_Height = 0;
 
 }; // Texture2D
