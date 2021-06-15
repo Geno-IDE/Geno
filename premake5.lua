6503dcb730496be00baec08a634164e7d2d3b560
Sebastian Kylander <developer@gaztin.com>
2021-06-16 00:37:15 +0200

    Apparently <filesystem> is still experimental

--- premake5.lua
+++ premake5.lua
@@ -45,6 +45,7 @@ app( 'Geno' )
 	filter 'system:linux'
 		exceptionhandling( "On" ) -- Need this for a temporary fix to memory errors in OutputWindow.cpp
 		links {
+			'stdc++fs',
 			'pthread',
 			'dl',
 			'GL',
