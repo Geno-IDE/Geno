c203a940b44f63c6eb1eaf726bcd195f7d0da18a
Sebastian Kylander <developer@gaztin.com>
2021-07-29 14:37:10 +0200

    Don't compile code that isn't needed

--- third_party/llvm-libllvm-ir.lua
+++ third_party/llvm-libllvm-ir.lua
@@ -11,16 +11,8 @@ return function()
 		'third_party/llvm-project/llvm/include',
 	}
 	files {
-		-- TODO: Check which of these don't depend on any TableGen'ed files, and thus can be moved into llvm-libllvm
-		'third_party/llvm-project/llvm/lib/Analysis/**.cpp',
-		'third_party/llvm-project/llvm/lib/Bitcode/**.cpp',
-		'third_party/llvm-project/llvm/lib/CodeGen/**.cpp',
-		'third_party/llvm-project/llvm/lib/DebugInfo/**.cpp',
 		'third_party/llvm-project/llvm/lib/IR/**.cpp',
-		'third_party/llvm-project/llvm/lib/Object/**.cpp',
 		'third_party/llvm-project/llvm/lib/ProfileData/**.cpp',
-		'third_party/llvm-project/llvm/lib/TextAPI/**.cpp',
-		'third_party/llvm-project/llvm/lib/Transforms/**.cpp',
 	}
 	removefiles {
 		'third_party/llvm-project/llvm/lib/DebugInfo/PDB/DIA/**.cpp',
