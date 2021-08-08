1c64af29b7b77bc1ba7e2928fad2cb4454a4ac77
unknown <theholyhorse.dev@gmail.com>
2021-08-09 01:40:55 +0200

    Fixed bug

--- src/Geno/C++/GUI/Widgets/TextEdit.cpp
+++ src/Geno/C++/GUI/Widgets/TextEdit.cpp
@@ -2303,9 +2303,6 @@ void TextEdit::EnterTextStuff( File& rFile, char C, int CursorIndex )
 
 	Line& rLine = rFile.Lines[ rCursor.Position.y ];
 
-	rCursor.Position.x++;
-	rCursor.SelectionOrigin = Coordinate( -1, -1 );
-
 	bool EndOfLine = rCursor.Position.x >= ( int )rLine.size();
 
 	if( EndOfLine )
@@ -2314,14 +2311,16 @@ void TextEdit::EnterTextStuff( File& rFile, char C, int CursorIndex )
 	}
 	else if( Props.CursorMode == CursorInputMode::Insert )
 	{
-		rLine[ rCursor.Position.x - 1 ].C = C;
+		rLine[ rCursor.Position.x ].C = C;
 	}
 	else
 	{
-		rLine.insert( rLine.begin() + rCursor.Position.x - 1, Glyph( C, m_Palette.Default ) );
+		rLine.insert( rLine.begin() + rCursor.Position.x, Glyph( C, m_Palette.Default ) );
 
 		AdjustCursors( rFile, CursorIndex, -1, 0 );
 	}
+
+	rCursor.Position.x++;
 
 	ScrollToCursor( rFile );
 
