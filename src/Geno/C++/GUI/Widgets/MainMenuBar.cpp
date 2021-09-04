0daeb4b2253cbdbb3cef245b4cb2834217dd4ec3
BEASTSM96 <beastboioioi.dev@gmail.com>
2021-09-04 11:53:37 +0100

    Fix Compile errors

--- src/Geno/C++/GUI/Widgets/MainMenuBar.cpp
+++ src/Geno/C++/GUI/Widgets/MainMenuBar.cpp
@@ -176,7 +176,7 @@ void MainMenuBar::ActionBuildBuildAndRun( void )
 
 		pWorkspace->Events.BuildFinished += []( Workspace& /*rWorkspace*/, std::filesystem::path OutputFile, bool /*Success*/ )
 		{
-			StatusBar::Instance().SetColor( StatusBarColor::ORANGE );
+			StatusBar::Instance().SetColor( StatusBar::StatusBarColor::ORANGE );
 
 			const std::string OutputString = OutputFile.string();
 
@@ -185,7 +185,7 @@ void MainMenuBar::ActionBuildBuildAndRun( void )
 			const int ExitCode = Process::ResultOf( OutputFile.wstring() );
 			std::cout << "=== " << OutputString << " finished with exit code " << ExitCode << " ===\n";
 
-			StatusBar::Instance().SetColor( StatusBarColor::RED );
+			StatusBar::Instance().SetColor( StatusBar::StatusBarColor::RED );
 		};
 
 		pWorkspace->Build();
